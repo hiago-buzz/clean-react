69471bf4612ccc386905425a9d34ebec62e11c95
Cicero Hiago Santos <CHISANTOS>
2021-03-25 18:05:35 -0300

    feat: ensure Remote authentication throw invalidCrentialsError on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,8 +1,8 @@
-
+import { HttpResponse } from './http-response'
 export type HttpPostParams = {
   url: string
   body?: object
 }
 export interface HttpPostClient {
-  post(params: HttpPostParams): Promise<void>
+  post(params: HttpPostParams): Promise<HttpResponse>
 }
