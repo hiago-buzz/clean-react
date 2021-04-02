040a79d7e0c6d059aa8f475cd8a105cf2d4e313d
Hiago Santos <foto.hiago@gmail.com>
2021-04-01 23:50:16 -0300

    chore: add babel-jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts, tsx}',
+    '<rootDir>/src/**/*.{ts,tsx}',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts,tsx)$': 'ts-jest'
+   "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    "\\.(scss|sass)$": "identity-obj-proxy"
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
